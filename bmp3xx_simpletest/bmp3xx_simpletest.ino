5f9e08e9f0c9654784c9f87ef9ea47b765fada1c
Kennosuke Nagata <114tukaiyasui@gmail.com>
2025-09-16 20:58:08 +0900

    over sampling

--- bmp3xx_simpletest/bmp3xx_simpletest.ino
+++ bmp3xx_simpletest/bmp3xx_simpletest.ino
@@ -34,22 +34,19 @@ void setup() {
   while (!Serial);
   Serial.println("Adafruit BMP388 / BMP390 test");
 
-  Wire.setSCL(5);
   Wire.setSDA(4);
-
-
-  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+  Wire.setSCL(5);
+  Wire.begin();
+  Wire.setClock(400000);
+  while (!bmp.begin_I2C()) {
     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    while (1);
   }
 
   // Set up oversampling and filter initialization
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  bmp.setTemperatureOversampling(BMP3_NO_OVERSAMPLING);
+  bmp.setPressureOversampling(BMP3_NO_OVERSAMPLING);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_DISABLE);
+  bmp.setOutputDataRate(BMP3_ODR_100_HZ);
 }
 
 void loop() {
@@ -70,5 +67,5 @@ void loop() {
   Serial.println(" m");
 
   Serial.println();
-  delay(2000);
+  delay(100);
 }
