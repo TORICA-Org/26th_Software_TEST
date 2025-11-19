861593ac7fe297c913c7bae032f3710dc52615a8
masashiyamamoto0809 <37615664+mss0809@users.noreply.github.com>
2025-11-19 12:33:04 +0900

    Update wav_test.ino
    unsinged 8bit or signed 16bit, サンプリング周波数16kHz音源へ対応，高音質化．またrp2040標準ライブラリを使用して外部ライブラリ不要化．

--- wav_test/wav_test.ino
+++ wav_test/wav_test.ino
@@ -1,26 +1,46 @@
-/*
-以下のライブラリを使用
-https://github.com/aloseed/RP2040-PWM-Audio/tree/main
+//rp2040の標準ライブラリを使用．外部ライブラリ不要
+//サンプリングレート16kHz，Unsigned 8bit raw
 
-*/
-#include "rp2040_pwm_audio.h"
+#include <PWMAudio.h>
 #include "wav.h"
 
-#define PIN_BUZZER 15
+//スピーカー用のピン
+#define speaker 15
 
-// 再生間隔(ms)
-#define PLAY_INTERVAL 3000
+PWMAudio pwm(speaker);
 
-void setup()
-{
-  // PWMオーディオ初期化(引数に出力ピンを指定)
-  pwm_audio_init(PIN_BUZZER);
-  // データ(rawデータ配列)をセット
-  set_sound(wav, sizeof(wav) / sizeof(wav[0]));
+const int audio_len = sizeof(raw_audio) / sizeof(raw_audio[0]);
+
+void setup() {
+  Serial.begin(115200);
+  
+  // サンプリングレート16kHzの場合
+  pwm.begin(16000);
+  
+  // バッファサイズ調整 (必要に応じて)
+  // pwm.setBuffers(4, 64); 
 }
 
-void loop()
-{
-  play_pwm_audio();
-  delay(PLAY_INTERVAL);
+void loop() {
+  Serial.println("Playing...");
+  
+  for (int i = 0; i < audio_len; i++) {
+    // 書き込みバッファに空きができるまで待機
+    while (pwm.availableForWrite() == 0) {
+      delayMicroseconds(10);
+    }
+    
+    // PWMAudioは16bit signed (-32768 ~ +32767) を期待するため変換
+    // 8bit unsigned (0~255) のデータの場合: (値 - 128) * 256
+    //8bit unsignedの場合
+    int16_t sample = (raw_audio[i] - 128) << 8;
+    pwm.write(sample);
+    
+
+    //16bit 音源int16_tの場合
+    //pwm.write((int16_t)raw_audio[i]);
+  }
+  
+  //再生終了後
+  delay(1000);
 }
